d048df4b8b9db46e716dd02dee520f5f0ffb705b
Yuji Nakamura <ny5117@icloud.com>
2023-04-24 10:45:31 +0900

    テスト環境の整備

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, InlineConfig, UserConfig } from "vite";
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +12,9 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 3000,
   },
-});
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setup.ts",
+  },
+} as VitestConfigExport);

17a19dcc94a248f83df9a13cc508adcac1e7a9a4
Yuji Nakamura <ny5117@icloud.com>
2023-04-26 09:15:24 +0900

    Tagコンポーネントを作成

---
+++ src/components/Elements/Tag/Tag.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import Tag from "./Tag";
+
+const meta = {
+  component: Tag,
+} satisfies Meta<typeof Tag>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: "All items",
+    styleType: "default",
+  },
+};
+
+export const Selected: Story = {
+  args: {
+    children: "men's clothing",
+    styleType: "selected",
+  },
+};
---
+++ src/components/Elements/Tag/Tag.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from "react";
+import styled from "styled-components";
+
+const BaseTag = styled.button`
+  padding: 5px 5px;
+  border-radius: 5px;
+  border: 1px solid #bbb;
+  outline: none;
+  cursor: pointer;
+
+  :focus {
+    outline: 0;
+  }
+`;
+
+const DefaultTag = styled(BaseTag)`
+  color: black;
+  background-color: transparent;
+`;
+
+const SelectedTag = styled(BaseTag)`
+  color: white;
+  background-color: #bbb;
+`;
+
+const tagStyleLists = {
+  default: DefaultTag,
+  selected: SelectedTag,
+};
+
+type PropsType = {
+  children: ReactNode;
+  styleType: "default" | "selected";
+};
+
+const Tag = ({ styleType, children }: PropsType) => {
+  const Component = tagStyleLists[styleType] || tagStyleLists.default;
+  return <Component>{children}</Component>;
+};
+
+export default Tag;
